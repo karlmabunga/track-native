92d104517556fae5312be62281818453f8135b45
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-18 15:50:38 -0700

    Added a track routes to send all the tracks for a certain user

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User')
+require('./models/Track')
 const express = require('express');
 const db = require('../db/index');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middleware/requireAuth');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 app.get('/', requireAuth, (req, res) => {
   res.send(`Your email is: ${req.user.email}`);
