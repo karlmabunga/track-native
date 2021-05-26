fc4a5e75582d5205d02e3cfa6926deb96f47620a
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-26 15:59:27 -0700

    The track list screen can now go to the track detail screen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const TrackListScreen = () => {
+const TrackListScreen = ({ navigation }) => {
   return (
+    <>
     <Text style={{ fontSize: 48 }} > TrackListScreen</Text>
+    <Button
+    title="Go to Track Detail"
+    onPress={() => { navigation.navigate('TrackDetail')}}
+    />
+    </>
   );
 };
 
