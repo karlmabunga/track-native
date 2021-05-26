2305f81528aadd099353d3c1a131b0343fba416e
Karl Mabunga <karlmabunga@yahoo.com>
2021-05-25 23:02:18 -0700

    Added a button to go to main flow with bottom tab navigator

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -6,6 +6,7 @@ const SignupScreen = ({ navigation}) => {
     <>
     <Text style={{ fontSize: 48 }} > SignupScreen</Text>
     <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}/>
+    <Button title="Go to main flow" onPress={() => navigation.navigate('mainFlow')}/>
     </>
   );
 };
